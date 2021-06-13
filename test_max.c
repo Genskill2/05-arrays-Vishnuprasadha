b1fef1b079eac9b04bc37e46dd5db7cf88d981e2
Vishnuprasadha <84711947+Vishnuprasadha@users.noreply.github.com>
2021-06-13 13:27:13 +0530

    Update test_max.c

--- test_max.c
+++ test_max.c
@@ -2,15 +2,7 @@
 #include <assert.h>
 
 
-int max(int nums[], int n){
-  int max_val=nums[0];
-  for(int i=1;i<n;i++){
-    if(max_val<nums[i]){
-      max_val=nums[i];
-    }
-  }
-  return max_val;
-}
+int max(int nums[], int n);
 
 int main(void) {
   int x[] = {9,5,6,10,2,-3,4};
