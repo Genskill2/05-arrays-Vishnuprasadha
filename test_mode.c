20362b42b1d52bcbc9a79e4cc336d3d3cee0e96c
Vishnuprasadha <84711947+Vishnuprasadha@users.noreply.github.com>
2021-06-13 10:09:51 +0530

    Update test_mode.c

--- test_mode.c
+++ test_mode.c
@@ -2,7 +2,21 @@
 #include <assert.h>
 
 
-int mode(int [], int);
+int mode(int nums[], int n){
+  int frequency=0, mode_count=0,mode_value=0;
+  for(int i=0;i<n;i++){
+    frequency=1;
+    for(int j=i+1;j<n;j++){
+      if(nums[i]==nums[j])
+        frequency++;
+    }
+    if(frequency>mode_count){
+      mode_count=frequency;
+      mode_value=nums[i];
+    }
+  }
+  return mode_value;
+}
 
 int main(void) {
   int x[] = {4, 9,5,6,5,10,0,2,-3, -3,4, 4};
