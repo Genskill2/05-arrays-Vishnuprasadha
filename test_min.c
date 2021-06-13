d332e5fa51bb5f9b2002ab0cefce7af7b3ec3eac
Vishnuprasadha <84711947+Vishnuprasadha@users.noreply.github.com>
2021-06-13 13:27:52 +0530

    Update test_min.c

--- test_min.c
+++ test_min.c
@@ -2,16 +2,7 @@
 #include <assert.h>
 
 
-int min(int nums[], int n){
-  int min_val=nums[0];
-  for(int i=1;i<n;i++){
-    if(nums[i]<min_val)
-      min_val=nums[i];
-  }
-  return min_val;
-
-}
-
+int min(int nums[], int n);
 int main(void) {
   int x[] = {9,5,6,10,2,-3,4};
   assert (min(x, 7) == -3);
