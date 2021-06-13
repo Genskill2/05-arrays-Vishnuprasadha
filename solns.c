30903b1cbec57279dda5bbe47f1c0ceb3d6dfa48
Vishnuprasadha <84711947+Vishnuprasadha@users.noreply.github.com>
2021-06-13 10:49:54 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,45 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+float average(int arr[], int n){
+  float average=0.0;
+  for(int i=0;i<n;i++){
+    average+=arr[i];
+  }
+  average=(float)average/(float)n;
+  return average;
+}
+int factors(int no,int nums[]){
+    int i=2;
+    int index=0;
+ while(no>=i){
+     if(no%i==0){
+         if(isprime(i)){
+         nums[index++]=i;
+         no=no/i;
+         }
+     }
+     else
+     i++;
+     
+ }
+return index;
+    
+}
+int max(int nums[], int n){
+  int max_val=nums[0];
+  for(int i=1;i<n;i++){
+    if(max_val<nums[i]){
+      max_val=nums[i];
+    }
+  }
+  return max_val;
+}
+int min(int nums[], int n){
+  int min_val=nums[0];
+  for(int i=1;i<n;i++){
+    if(nums[i]<min_val)
+      min_val=nums[i];
+  }
+  return min_val;
+
+}
